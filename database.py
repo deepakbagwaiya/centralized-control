cf73c64237b16a5eaa4b484aa8caeda762efcba8
deepakbagwaiya <deepakbagwaiya3@gmail.com>
2026-04-08 19:57:39 +0530

    Add files via upload

--- database.py
+++ database.py
@@ -1,15 +1,19 @@
 import mysql.connector
 import streamlit as st
+import certifi
 
 @st.cache_resource
 def get_db_connection():
     try:
         return mysql.connector.connect(
-            host="127.0.0.1",
-            user="root",
-            password="",
-            database="deeprail",
-            port=3306
+            host=st.secrets["DB_HOST"],
+            user=st.secrets["DB_USER"],
+            password=st.secrets["DB_PASS"],
+            database=st.secrets["DB_NAME"],
+            port=st.secrets["DB_PORT"],
+            ssl_verify_cert=True,
+            ssl_verify_identity=True,
+            ssl_ca=certifi.where()   # <--- This securely verifies the connection!
         )
     except mysql.connector.Error as err:
         st.error(f"Database connection failed: {err}")
